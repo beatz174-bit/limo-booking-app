7587ea5048e359b801d5407f390e0c91f4b2cf93
beatzaplenty <wayne.bennett@live.com>
2025-08-11 16:22:34 +1000

    modified:   frontend/.env.development 	modified:   frontend/.env.production 	modified:   frontend/src/pages/PageNotFound.tsx 	modified:   frontend/vite.config.ts

--- frontend/src/pages/PageNotFound.tsx
+++ frontend/src/pages/PageNotFound.tsx
@@ -2,16 +2,16 @@
 import { Link } from 'react-router-dom';
 import taxi404 from '../assets/404.png'; // adjust path as needed
 
-const cdnBase = import.meta.env.VITE_CDN_BASE_URL;
+// const cdnBase = import.meta.env.VITE_CDN_BASE_URL;
 const PageNotFound = () => {
-      const imageSrc =
-    import.meta.env.PROD && cdnBase
-      ? `${cdnBase}/images/404.png`
-      : taxi404;
+    //   const imageSrc =
+    // import.meta.env.PROD && cdnBase
+    //   ? `${cdnBase}/images/404.png`
+    //   : taxi404;
   return (
     <div className="page-not-found">
       <h1>404 – We Took a Wrong Turn!</h1>
-      <img src={imageSrc} alt="Cartoon limo blowing steam - 404 error" className="page-not-found__image" height={400} width={400}/>
+      <img src={taxi404} alt="Cartoon limo blowing steam - 404 error" className="page-not-found__image" height={400} width={400}/>
       <p>Oops! The page you're looking for can’t be found.</p>
       <Link to="/book" className="page-not-found__home-link">← Back to Home</Link>
     </div>
