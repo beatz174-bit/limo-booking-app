1b02a38f346d633a34a1f7edd3b2637a4fbbd1d3
beatzaplenty <wayne.bennett@live.com>
2025-08-18 17:05:21 +1000

    modified:   frontend/tsconfig.app.json 	new file:   frontend/tsconfig.base.json 	new file:   frontend/tsconfig.vitest.json 	modified:   frontend/vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,9 +2,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath } from "node:url";
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
