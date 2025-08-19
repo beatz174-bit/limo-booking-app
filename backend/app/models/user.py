a13f25ceded893ba22a81017604b0485270a35a4
beatzaplenty <wayne.bennett@live.com>
2025-08-20 00:56:38 +1000

    change pipe to union 	modified:   backend/app/models/user.py

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -3,7 +3,7 @@
 # from sqlalchemy import Boolean
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import Text
-from typing import Literal
+from typing import Literal, Union
 from app.db.database import Base
 
 Status = Literal["pending", "accepted", "completed", "cancelled"]
@@ -22,6 +22,6 @@ class User(Base):
     # Password hashed with chosen algorithm
     hashed_password: Mapped[str]    = mapped_column(Text, name="password_hash")
     # Optional default pickup address for quick bookings
-    default_pickup_address: Mapped[str | None] = mapped_column(Text, nullable=True, default=None)
+    default_pickup_address: Mapped[Union[str, None]] = mapped_column(Text, nullable=True, default=None)
     # is_approved:     Mapped[bool]   = mapped_column(Boolean, default=False)
     # role:            Mapped[Status] = mapped_column(Text, default="pending")
