ad2622077ef3bf301d341ada9e505c1444c9fc07
beatzaplenty <wayne.bennett@live.com>
2025-09-10 20:13:14 +1000

    Revert "Merge pull request #474 from beatz174-bit/codex/find-missing-onesignal-cdn-import"
    This reverts commit cccf3fd039a05bb19a74ec4f59b66f09bd9d78e2, reversing
    changes made to b783bb10287fd465d720b9fe3e8f50e812dcebaa.

--- frontend/public/OneSignalSDKWorker.js
+++ frontend/public/OneSignalSDKWorker.js
@@ -1,1 +1,7 @@
+<<<<<<< HEAD
 importScripts('/OneSignalSDK.sw.js');
+=======
+importScripts(
+  'https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.worker.js'
+);
+>>>>>>> parent of cccf3fd (Merge pull request #474 from beatz174-bit/codex/find-missing-onesignal-cdn-import)
